47afa545ea10cd56d9083fceb0cca49361bad666
Qjoyboy <khanmagomedovruslan77@gmail.com>
2024-10-03 17:50:00 +0300

    added categories and reverse to product by id

--- shop/models.py
+++ shop/models.py
@@ -1,6 +1,7 @@
 from django import forms
 
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from shop import views
 
+app_name='shop'
 urlpatterns = [
-    path('',views.IndexView.as_view(), name='index')
+    path('',views.CategoryListView.as_view(), name='category'),
+    path('products/<int:pk>/', views.ProductListView.as_view(), name='product'),
 ]
