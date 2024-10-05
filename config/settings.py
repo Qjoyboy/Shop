8e0db0ceaf7d44f17e0a0e3890a74b6d978ede94
Qjoyboy <khanmagomedovruslan77@gmail.com>
2024-10-06 00:23:57 +0300

    added some CRUD operations, refactored templates, and some tiny fixes

--- config/settings.py
+++ config/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
-from django.conf.global_settings import MEDIA_ROOT, MEDIA_URL, STATICFILES_DIRS, DATE_INPUT_FORMATS
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -125,12 +123,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'shop/static/'
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static'
-# ]
-#
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = ''
+STATIC_ROOT = (BASE_DIR / 'static')
+
+STATICFILES_DIRS = [BASE_DIR / 'shop/static']
+
+MEDIA_ROOT = (BASE_DIR / 'media')
+MEDIA_URL = '/media/'
 
 
 # Default primary key field type
